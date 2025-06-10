e958a18018fa4f5914b4dd55f01eba38d6f535fb
MC <2281034748@qq.com>
2025-06-10 21:31:41 +0800

    no message

--- config/env/dev.js
+++ config/env/dev.js
@@ -4,13 +4,13 @@
 export default {
     // API基础路径
     // baseUrl: 'http://103.38.83.91:8080',
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://103.38.83.91:8080',
     // 超时时间
     timeout: 100000,
     // 上传接口
     uploadUrl: '/api/upload',
     // WebSocket地址
-    wsUrl: 'ws://localhost:8080/ws',
+    wsUrl: 'ws://103.38.83.91:8080/ws',
     // 调试模式
     debug: true,
     // 版本号
